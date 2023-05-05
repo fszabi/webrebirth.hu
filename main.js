938e9c143ff5c93935fdca60bdc8f6f2a4a983f1
Airstonex <szabolcsforgacs2@gmail.com>
2023-05-05 17:06:35 +0200

    webdesight hamburger added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const header = document.body.querySelector(".primary-header");
 
 const headerFlex = document.body.querySelector(".primary-header .flex-group");
 
+const navFlex = document.body.querySelector(".primary-navigation .flex-group");
+
 const hamburger = document.querySelector(".hamburger");
 
 const navlist = document.querySelector("ul");
@@ -37,16 +39,24 @@ function transparent() {
 }
 
 function changeAlignment() {
-  if (window.innerWidth >= 680) {
+  if (window.innerWidth >= 940) {
     headerFlex.classList.remove("justify-center");
     headerFlex.classList.remove("flex-col");
     headerFlex.classList.add("justify-between");
     headerFlex.classList.add("flex-row");
-  } else if (window.innerWidth < 680) {
+  } else if (window.innerWidth < 940) {
     headerFlex.classList.remove("justify-between");
     headerFlex.classList.remove("flex-row");
     headerFlex.classList.add("justify-center");
     headerFlex.classList.add("flex-col");
+  }
+
+  if (window.innerWidth >= 500) {
+    navFlex.classList.remove("flex-col");
+    navFlex.classList.add("flex-row");
+  } else if (window.innerWidth < 500) {
+    navFlex.classList.remove("flex-row");
+    navFlex.classList.add("flex-col");
   }
 }
 
