646a3e86502795be5e34fc2ffa7ac45b3f24a48a
Airstonex <szabolcsforgacs2@gmail.com>
2023-05-11 13:26:37 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import htmlPurge from "vite-plugin-purgecss";
+import path from "path";
 
 export default {
   plugins: [htmlPurge()],
   base: "",
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        sent: path.resolve(__dirname, "sent.html"),
+      },
+    },
+  },
 };
