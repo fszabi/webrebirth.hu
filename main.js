2506c62e93b2eb33908250b40266452b60cc0152
Airstonex <szabolcsforgacs2@gmail.com>
2023-05-18 11:33:39 +0200

    updates

--- main.js
+++ main.js
@@ -1,51 +1,87 @@
-const header = document.querySelector(".primary-header");
-
-const headerFlex = document.querySelector(".primary-header .flex-group");
-
-const navcontainer = document.querySelector(".primary-navigation-container");
-
-const nav = document.querySelector(".primary-navigation");
-
-const navFlex = document.querySelector(".primary-navigation .flex-group");
-
-const hamburger = document.querySelector(".btn--hamburger");
-
-const navlist = document.querySelector("ul");
-
-const navlinks = document.body.querySelectorAll(".primary-navigation a");
-
-const logo = document.body.querySelector(".logo");
-
-const scrollButton = document.querySelector(".btn--scroll-top");
-
-window.onscroll = function () {
-  navColor();
-  toggleScrollButton();
-};
+// window.onscroll = function () {
+//   toggleScrollButton();
+//   navColor();
+// };
 
 window.onload = navEvents;
 
 // Header
 
-function navColor() {
-  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
-    black();
-  } else {
-    transparent();
-  }
-}
+const header = document.querySelector(".primary-header");
 
-function black() {
-  header.style.backgroundColor = "#212121";
-  header.classList.add("box-shadow-1");
-}
+const scrollWatcher = document.createElement("div");
 
-function transparent() {
-  header.style.backgroundColor = "transparent";
-  header.classList.remove("box-shadow-1");
-}
+const scrollButton = document.querySelector(".btn--scroll-top");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+
+header.before(scrollWatcher);
+
+const headerObserver = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+
+    // Toggle header color and box-shadow
+
+    header.classList.toggle("sticky", !entries[0].isIntersecting);
+    header.classList.toggle("box-shadow-1", !entries[0].isIntersecting);
+  },
+  { rootMargin: "200px 0px 0px 0px" }
+);
+
+headerObserver.observe(scrollWatcher);
+
+// Scroll top button
+
+const scrollButtonObserver = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+
+    // Toggle scrollButton visibility
+
+    scrollButton.classList.toggle("visible", !entries[0].isIntersecting);
+
+    scrollButton.addEventListener("click", () => {
+      scrollTop();
+    });
+
+    function scrollTop() {
+      document.body.scrollTop = 0; // Safari
+      document.documentElement.scrollTop = 0; // Firefox, Chrome, Opera
+    }
+  },
+  { rootMargin: "2000px 0px 0px 0px" }
+);
+
+scrollButtonObserver.observe(scrollWatcher);
+
+// function navColor() {
+//   if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+//     black();
+//   } else {
+//     transparent();
+//   }
+// }
+
+// function black() {
+//   header.style.backgroundColor = "var(--clr-neutral-900)";
+//   header.classList.add("box-shadow-1");
+// }
+
+// function transparent() {
+//   header.style.backgroundColor = "var(--clr-transparent)";
+//   header.classList.remove("box-shadow-1");
+// }
 
 // Hamburger menu
+
+const navcontainer = document.querySelector(".primary-navigation-container");
+
+const nav = document.querySelector(".primary-navigation");
+
+const hamburger = document.querySelector(".btn--hamburger");
+
+const navlinks = document.body.querySelectorAll(".primary-navigation a");
 
 function navEvents() {
   for (var i = 0; i < navlinks.length; i++) {
@@ -118,32 +154,34 @@ function showSlides() {
 
 // Scroll top button
 
-function toggleScrollButton() {
-  if (
-    document.body.scrollTop > 1000 ||
-    document.documentElement.scrollTop > 1000
-  ) {
-    toggleOn();
-  } else {
-    toggleOff();
-  }
-}
+// const scrollButton = document.querySelector(".btn--scroll-top");
 
-function toggleOn() {
-  scrollButton.style.visibility = "visible";
-  scrollButton.style.opacity = "0.7";
-}
+// function toggleScrollButton() {
+//   if (
+//     document.body.scrollTop > 1000 ||
+//     document.documentElement.scrollTop > 1000
+//   ) {
+//     toggleOn();
+//   } else {
+//     toggleOff();
+//   }
+// }
 
-function toggleOff() {
-  scrollButton.style.visibility = "hidden";
-  scrollButton.style.opacity = "0";
-}
+// function toggleOn() {
+//   scrollButton.style.visibility = "visible";
+//   scrollButton.style.opacity = "0.7";
+// }
 
-scrollButton.addEventListener("click", () => {
-  scrollTop();
-});
+// function toggleOff() {
+//   scrollButton.style.visibility = "hidden";
+//   scrollButton.style.opacity = "0";
+// }
 
-function scrollTop() {
-  document.body.scrollTop = 0; // Safari
-  document.documentElement.scrollTop = 0; // Firefox, Chrome, Opera
-}
+// scrollButton.addEventListener("click", () => {
+//   scrollTop();
+// });
+
+// function scrollTop() {
+//   document.body.scrollTop = 0; // Safari
+//   document.documentElement.scrollTop = 0; // Firefox, Chrome, Opera
+// }
