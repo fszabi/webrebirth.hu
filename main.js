1015b0f1644c63c85c6528ad05b2ef168e4bd6ed
Airstonex <szabolcsforgacs2@gmail.com>
2023-05-27 19:32:57 +0200

    lazy images

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const faders = document.querySelectorAll(".fade-in");
 
 const sliders = document.querySelectorAll(".slide-in");
 
+const lazyImages = document.querySelectorAll("[data-src]");
+
 // Header
 
 scrollWatcher.setAttribute("data-scroll-watcher", "");
@@ -88,6 +90,37 @@ faders.forEach((fader) => {
 
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
+});
+
+// Lazy load images
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  } else {
+    img.src = src;
+  }
+}
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 300px 0px",
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+lazyImages.forEach((image) => {
+  imgObserver.observe(image);
 });
 
 // Events on load
