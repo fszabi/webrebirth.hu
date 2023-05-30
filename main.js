5b6f690ce1ae73bcddae914fdda8b183c9128d28
Airstonex <szabolcsforgacs2@gmail.com>
2023-05-30 14:47:09 +0200

    updated services

--- main.js
+++ main.js
@@ -94,34 +94,34 @@ sliders.forEach((slider) => {
 
 // Lazy load images
 
-function preloadImage(img) {
-  const src = img.getAttribute("src");
-  if (!src) {
-    return;
-  } else {
-    img.src = src;
-  }
-}
+// function preloadImage(img) {
+//   const src = img.getAttribute("src");
+//   if (!src) {
+//     return;
+//   } else {
+//     img.src = src;
+//   }
+// }
 
-const imgOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 300px 0px",
-};
+// const imgOptions = {
+//   threshold: 0,
+//   rootMargin: "0px 0px 300px 0px",
+// };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      preloadImage(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  });
-}, imgOptions);
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     } else {
+//       preloadImage(entry.target);
+//       imgObserver.unobserve(entry.target);
+//     }
+//   });
+// }, imgOptions);
 
-lazyImages.forEach((image) => {
-  imgObserver.observe(image);
-});
+// lazyImages.forEach((image) => {
+//   imgObserver.observe(image);
+// });
 
 // Events on load
 
