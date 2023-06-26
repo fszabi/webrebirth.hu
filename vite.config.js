571b3a33907a4d7a1bdc445f12bdbe48071b2492
Airstonex <szabolcsforgacs2@gmail.com>
2023-06-26 14:38:54 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        success: resolve(__dirname, "success.html"),
       },
     },
   },
